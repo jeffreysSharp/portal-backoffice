b1d30c3f8a0fb12875126458fea9266fc8bae2b1
Jeferson Almeida <jeferson.almeida@resourceit.com>
2019-09-12 21:53:37 -0300

    start-redirect-to-login

--- src/app/views/sessions/sessions.routing.ts
+++ src/app/views/sessions/sessions.routing.ts
@@ -1,17 +1,17 @@
-import { Signup4Component } from './signup4/signup4.component';
-import { Signup3Component } from './signup3/signup3.component';
-import { Signup2Component } from './signup2/signup2.component';
 import { Routes } from "@angular/router";
-
+import { ErrorComponent } from "./error/error.component";
 import { ForgotPasswordComponent } from "./forgot-password/forgot-password.component";
 import { LockscreenComponent } from "./lockscreen/lockscreen.component";
-import { SigninComponent } from "./signin/signin.component";
-import { SignupComponent } from "./signup/signup.component";
 import { NotFoundComponent } from "./not-found/not-found.component";
-import { ErrorComponent } from "./error/error.component";
+import { SigninComponent } from "./signin/signin.component";
+import { Signin2Component } from './signin2/signin2.component';
 import { Signin3Component } from './signin3/signin3.component';
 import { Signin4Component } from './signin4/signin4.component';
-import { Signin2Component } from './signin2/signin2.component';
+import { SignupComponent } from "./signup/signup.component";
+import { Signup2Component } from './signup2/signup2.component';
+import { Signup3Component } from './signup3/signup3.component';
+import { Signup4Component } from './signup4/signup4.component';
+
 
 export const SessionsRoutes: Routes = [
   {
@@ -22,6 +22,7 @@ export const SessionsRoutes: Routes = [
         component: SignupComponent,
         data: { title: "Signup" }
       },
+
       {
         path: "signup2",
         component: Signup2Component,
